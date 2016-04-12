5537b8c5a9950d8241d8fb303ac664aa232c0bd3
austinminnon <whi7ee@gmail.com>
2016-04-12 14:21:01 -0700

    add delete function for answer

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+actions: {
+  delete(answer) {
+    if(confirm("are you sure?")) {
+    this.sendAction('delete', answer);
+      }
+    }
+  }
 });
