3b49ec468b96ca3a30e17acd9625e80eb84c4e3f
austinminnon <whi7ee@gmail.com>
2016-04-12 14:10:39 -0700

    add helper for displaying <1 x >1 check and >3 fire

--- app/helpers/questions-popularity.js
+++ app/helpers/questions-popularity.js
@@ -4,6 +4,10 @@ export function questionsPopularity(params) {
   var question = params[0];
   if(question.get('answers').get('length') >=3) {
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+  }else if (question.get('answers').get('length') >1) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-ok-circle"><span>')
+  }else {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-remove-circle"><span>')
   }
 }
 
